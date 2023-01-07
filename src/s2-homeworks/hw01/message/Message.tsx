e7c324f8ef461df958bb6c3ebd9e40a56f08d255
vansam1985 <vansam1985@yandex.ru>
2023-01-07 13:52:21 +0300

    Homeworks 1

---
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {messageType, userType} from '../HW1'
+import s from './Message.module.css'
+
+// нужно создать правильный тип вместо any
+export type MessagePropsType = {
+    message: ObjectType
+
+}
+type ObjectType = {
+    id: number
+    user: userType
+    message: messageType
+}
+
+
+// нужно отобразить приходящие данные
+const Message = (props: MessagePropsType) => {
+    return (
+        <div id={'hw1-message-' + props.message.id} className={s.message}>
+            <div className={s.imageAndText}>
+                <div className={s.text}>
+                    <div id={'hw1-name-' + props.message.id} className={s.name}>
+                        {props.message.user.name}
+
+                    </div>
+                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
+                        {props.message.message.text}
+                    </pre>
+                </div>
+                <img className={'avatar'}
+                     id={'hw1-avatar-' + props.message.id}
+                     src={props.message.user.avatar}
+                />
+            </div>
+            <div id={'hw1-time-' + props.message.id} className={s.time}>
+                {props.message.message.time}
+            </div>
+        </div>
+    )
+}
+
+export default Message
